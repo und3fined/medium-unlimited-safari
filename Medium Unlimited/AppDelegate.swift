009d6d8d62d9fc07e331182ccd832d3fa7ab3711
und3fined <cuonglee1011@gmail.com>
2020-09-14 16:28:34 +0700

    Initial Commit

---
+++ Medium Unlimited/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Medium Unlimited
+//
+//  Created by und3fined on 9/14/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
